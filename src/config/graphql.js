592525e887016f56e2a4cfe3422e8ca7ca6b1e35
Ali Daniel Febres T <alidanft@gmail.com>
2021-05-22 12:08:24 +0200

    reset template to fix eol

--- src/config/graphql.js
+++
@@ -1,9 +0,0 @@
-import {GeneralConstants} from '@utils/constants';
-
-const graphqlConfig = {
-  port: 3000,
-  endpoint: (process.env.NODE_ENV === GeneralConstants.DEVELOPMENT && '/') || '/api',
-  playground: (process.env.NODE_ENV === GeneralConstants.DEVELOPMENT && '/graphql') || false
-};
-
-export default graphqlConfig;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
-/* eslint-disable no-console */
-import {GraphQLServer} from 'graphql-yoga';
-import {config} from 'dotenv-safe';
-
-import graphqlConfig from '@config/graphql';
-import {typeDefs, resolvers} from '@graphql';
-import {GeneralConstants} from '@utils/constants';
+import { config } from 'dotenv-safe'
 
 config({
-  example: '.env'
-});
-
-const server = new GraphQLServer({
-  typeDefs,
-  resolvers
-});
-
-server.start(graphqlConfig, ({port}) => {
-  if (process.env.NODE_ENV === GeneralConstants.PRODUCTION) {
-    console.log('Server listening', `http://localhost:${port}/api`);
-  } else {
-    console.log('Server listening', `http://localhost:${port} with playground in /graphql`);
-  }
-});
+  example: '.env',
+})
